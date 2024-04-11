a2745642e286d5d6284e762e48e34611423d00ed
anastasiiaut <124287939+anastasiiaut@users.noreply.github.com>
2024-04-11 19:11:39 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,33 +3,33 @@
 #include "Automata.h"
 
 int main() {
- setlocale(LC_ALL, "Russian");
- Automata CoffeMachine;
- CoffeMachine.on();
- CoffeMachine.getMenu();
- int input;
- std::cout << "Введите сумму (в рублях): ";
- std::cin >> input;
- CoffeMachine.coin(input);
+setlocale(LC_ALL, "Russian");
+Automata CoffeMachine;
+CoffeMachine.on();
+CoffeMachine.getMenu();
+int input;
+std::cout << "Введите сумму (в рублях): ";
+std::cin >> input;
+CoffeMachine.coin(input);
 
- std::cout << "Выберите напиток из меню (введите номер): ";
- std::cin >> input;
- CoffeMachine.choice(input);
+std::cout << "Выберите напиток из меню (введите номер): ";
+std::cin >> input;
+CoffeMachine.choice(input);
 
- std::cout << std::endl;
+std::cout << std::endl;
 
- std::cout << "Введите сумму (в рублях): ";
- std::cin >> input;
- CoffeMachine.coin(input);
+std::cout << "Введите сумму (в рублях): ";
+std::cin >> input;
+CoffeMachine.coin(input);
 
- std::cout << "Выберите напиток из меню (введите номер): ";
- std::cin >> input;
- CoffeMachine.choice(input);
+std::cout << "Выберите напиток из меню (введите номер): ";
+std::cin >> input;
+CoffeMachine.choice(input);
 
- std::cout << std::endl;
+std::cout << std::endl;
 
- CoffeMachine.cancel();
- std::cout << CoffeMachine.getState() << std::endl;
- CoffeMachine.off();
- return 0;
+CoffeMachine.cancel();
+std::cout << CoffeMachine.getState() << std::endl;
+CoffeMachine.off();
+return 0;
 }
